eb82356dc9e8755182db7a07e450ed2d55bfebf4
Markazi.co <Markazi.co@LAPTOP-VA2L6RTV>
2023-03-26 14:42:22 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Araye_Ekhtelaf
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Student Nomre = new Student();
+            int nomreUSER;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Nomre Daneshjoo shomare " +(i+1)+ " Ro vared kon ");
+                 nomreUSER=int.Parse(Console.ReadLine());
+                Nomre.insert(nomreUSER);
+                Console.Clear();
+            }
+            Console.Clear();
+            Console.WriteLine("Enter a number to search in Array :");
+            int SearchNumber = int.Parse(Console.ReadLine());
+            var nearest = Nomre.ArayeNomre.MinBy(x => Math.Abs((long)x - SearchNumber));
+Console.WriteLine("Result Is " +  Math.Abs(SearchNumber-nearest));
+            Console.ReadKey();
+        }
+    }
+    class Student
+    {
+        public int[] ArayeNomre = new int[10];
+        int i = 0;
+        public void insert(int nomre)
+        {
+            ArayeNomre[i] = nomre;
+            i++;
+        }
+    } 
+}
